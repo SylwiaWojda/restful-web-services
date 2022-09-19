cbd736256169d5669aca8733f280abc94049ab08
SylwiaWojda <sylwiaaa001@wp.pl>
2022-09-19 15:41:44 +1000

    POST create user

--- src/main/java/com/igreendata/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/igreendata/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -10,12 +10,16 @@ import java.util.function.Predicate;
 @Component
 public class UserDaoService {
 
+    private static int userCount = 0;
     private static List<User> users = new ArrayList<>();
     static {
-        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Eve", LocalDate.now().minusYears(20)));
-        users.add(new User(3, "Jordan", LocalDate.now().minusYears(40)));
+        users.add(new User(++userCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCount, "Eve", LocalDate.now().minusYears(20)));
+        users.add(new User(++userCount, "Jordan", LocalDate.now().minusYears(40)));
     }
+
+
+
     public List<User> findAll() {
         return users;
     }
@@ -23,6 +27,12 @@ public class UserDaoService {
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals((id));
         return users.stream().filter(predicate).findFirst().get();
+    }
+
+    public User save(User user) {
+        user.setId(++userCount);
+        users.add(user);
+        return user;
     }
 
 }
